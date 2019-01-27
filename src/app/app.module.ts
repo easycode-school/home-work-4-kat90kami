343a20bb54e27e8bee9a9199482cec883a5d7c87
kat90kami <kat90kami@gmail.com>
2019-01-28 00:00:03 +0200

    homework 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		CarComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-car',
+	templateUrl: './car.component.html',
+	styleUrls: ['./car.component.css']
+})
+export class CarComponent {
+	public car = {
+		name: 'Tesla',
+		mileage: 100,
+		tank: 100,
+		gas: 30,
+		specifications: ['Двигатель 2 литра', 'максимальная скорость 200 км/ч']
+	};
+
+	public visible = false;
+	/**
+	 * метод drive увеличиват пробег машины и уменьшат бензин в баке + активирует кнопку Refuel если бензин подходит к нулю (< 10)
+	 */
+	drive() {
+		this.car.mileage += 10;
+		this.car.gas--;
+		if (this.car.gas <= 10) {
+			this.visible = true;
+		}
+		console.log(this.car)
+	}
+	/**
+	 * метод refuel обновляет значение бензина в баке
+	 */
+	refuel() {
+		this.car.gas = 30;
+		this.visible = false;
+	}
+}  	
